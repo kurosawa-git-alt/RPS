7a003be7d4cdeb71f52d11919ce6e44f47f360c9
EC2 Default User <ec2-user@ip-172-31-24-254.us-east-2.compute.internal>
2021-03-16 06:27:07 +0000

    comp

--- rps.rb
+++ rps.rb
@@ -1,39 +1,67 @@
-puts "じゃんけん..."
-def janken
-    puts "0(グー)1(チョキ)2(パー)3(戦わない)"
 
-    myself = gets.to_i
-    opponent = rand(3)
+    puts "じゃんけん..."
+    def janken
+        puts "0(グー)1(チョキ)2(パー)3(戦わない)"
 
-    waza = ["グー","チョキ","パー","逃げる"]
+        myself = gets.to_i
+        opponent = rand(3)
 
-    puts "ホイ！"
-    puts "---------------"
-    puts "あなた :#{waza[myself]}を選択しました"
-    if myself == 3
-        puts "戦いを放棄しました"
-    else
-        puts "相手 :#{waza[opponent]}を出しました"
+        waza = ["グー","チョキ","パー","逃げる"]
+
+        puts "ホイ！"
+        puts "---------------"
+        puts "あなた :#{waza[myself]}を選択しました"
+        if myself == 3
+            puts "戦いを放棄しました"
+        else
+            puts "相手 :#{waza[opponent]}を出しました"
+        end
+        puts "---------------"
+
+        if myself == opponent
+            puts "あいこで..."
+            return true
+        elsif(myself == 0 && opponent == 1) || (myself == 1 && opponent == 2) || (myself == 2 && opponent == 0)
+            puts "あっち向いて..."
+            puts "0(上)1(下)2(右)3(左)ー指を差す方向を決める"
+            mdirection = gets.to_i
+            oface = rand(4)
+            direction = ["上","下","右","左"]
+            puts "ホイ！"
+            puts "----------------"
+            puts "あなた :#{direction[mdirection]}を選択しました"
+            puts "相手 :#{direction[oface]}を向きました"
+            if mdirection == oface
+                puts "あなたの勝ちです"
+            else
+                puts "じゃんけん..."
+                return true
+            end
+        elsif myself == 3
+            puts "逃亡に成功しました"
+            return false
+        else
+            puts "あっち向いて..."
+            puts "0(上)1(下)2(右)3(左)ー顔を動かす方向を決める"
+            mdirection = gets.to_i
+            oface = rand(4)
+            direction = ["上","下","右","左"]
+            puts "ホイ！"
+            puts "----------------"
+            puts "あなた :#{direction[mdirection]}を選択しました"
+            puts "相手 :#{direction[oface]}を向きました"
+            if mdirection == oface
+                puts "あなたの勝ちです"
+            else
+                puts "じゃんけん..."
+                return true
+            end
+              
+        end
     end
-    puts "---------------"
 
-    if myself == opponent
-        puts "あいこで..."
-        return true
-    elsif(myself == 0 && opponent == 1) || (myself == 1 && opponent == 2) || (myself == 2 && opponent == 0)
-        puts "あなたの勝ちです"
-        return false
-    elsif myself == 3
-        puts "逃亡に成功しました"
-        return false
-    else
-        puts "あなたの負けです"
-        return false
+    again = true
+
+    while again
+        again = janken
     end
-end
-
-again = true
-
-while again
-    again = janken
-end
