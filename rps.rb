abf406e27e81b14e83ab4cbe0f66972736fc521e
EC2 Default User <ec2-user@ip-172-31-24-254.us-east-2.compute.internal>
2021-03-16 05:22:44 +0000

    1

--- rps.rb
+++ rps.rb
@@ -1,9 +1,39 @@
 puts "じゃんけん..."
-puts "0(グー)1(チョキ)2(パー)3(戦わない)"
+def janken
+    puts "0(グー)1(チョキ)2(パー)3(戦わない)"
 
-waza = gets.to_i
+    myself = gets.to_i
+    opponent = rand(3)
 
+    waza = ["グー","チョキ","パー","逃げる"]
 
-puts "ホイ！"
-puts "---------------"
-puts "あなた :#{waza}を出しました"
+    puts "ホイ！"
+    puts "---------------"
+    puts "あなた :#{waza[myself]}を選択しました"
+    if myself == 3
+        puts "戦いを放棄しました"
+    else
+        puts "相手 :#{waza[opponent]}を出しました"
+    end
+    puts "---------------"
+
+    if myself == opponent
+        puts "あいこで..."
+        return true
+    elsif(myself == 0 && opponent == 1) || (myself == 1 && opponent == 2) || (myself == 2 && opponent == 0)
+        puts "あなたの勝ちです"
+        return false
+    elsif myself == 3
+        puts "逃亡に成功しました"
+        return false
+    else
+        puts "あなたの負けです"
+        return false
+    end
+end
+
+again = true
+
+while again
+    again = janken
+end
